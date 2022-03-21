f4b1a6754c5b639ffa44f6ad4530ba67fe99e924
rivailjunior <rivail.rj@gmail.com>
2022-03-20 20:03:33 -0400

    fix(test): container test

--- src/components/container/container.test.tsx
+++ src/components/container/container.test.tsx
@@ -6,7 +6,7 @@ const Component = () => <div className="box">box</div>;
 describe("Container", () => {
   test("should render container properly", () => {
     render(<Container />);
-    expect(screen.getByRole("container", { name: "" })).toBeInTheDocument();
+    expect(screen.getByRole("container-row", { name: "" })).toBeInTheDocument();
   });
 
   test("should render container with itens inside properly", () => {
